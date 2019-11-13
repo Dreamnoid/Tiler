3b88233d0c65d9ec7c55b7425a88cb39a9a36b45
Dreamnoid <Dreamnoid@users.noreply.github.com>
2019-11-13 14:19:59 +0100

    Initial commit

---
+++ src/Editor.cs
@@ -0,0 +1,144 @@
+﻿using System.Windows.Forms;
+using GameLib;
+using GameLib.Editor;
+
+namespace Tiler
+{
+    class Editor : IOp
+    {
+
+        private static IMGUI UI = new IMGUI(new SPFPlatform()) {Font = Game.Font};
+
+        private int _tileSize = 16;
+        private Color _currentColor = Color.Black;
+        private int _pixelSize = 12;
+
+        private Map<Color> _image;
+
+        public OpState Start()
+        {
+            _image = new Map<Color>(16, 16);
+            return OpState.Running;
+        }
+
+        public OpState Update(float dt)
+        {
+            var layout = Game.Screen;
+            layout = UI.ToolGroup(layout, "Pixel");
+
+            var toolbar = Layout.StackV(ref layout, 25, 2);
+            if (UI.Button(Layout.StackH(ref toolbar, 250, 2), "From Clipboard"))
+            {
+                if (Clipboard.ContainsImage())
+                {
+                    _image = Utils.FromBitmap(Clipboard.GetImage());
+                }
+            }
+
+            if (UI.Button(Layout.StackH(ref toolbar, 250, 2), "To Clipboard"))
+            {
+                using (var bmp = Utils.ToBitmap(_image))
+                {
+                    Clipboard.SetImage(bmp);
+                }
+            }
+
+            Layout.StackH(ref toolbar, 20);
+            UI.RadioButton(Layout.StackH(ref toolbar, 50), "4", 4, ref _pixelSize);
+            UI.RadioButton(Layout.StackH(ref toolbar, 50), "8", 8, ref _pixelSize);
+            UI.RadioButton(Layout.StackH(ref toolbar, 50), "12", 12, ref _pixelSize);
+
+            var paletteGroup = UI.ToolGroup(Layout.CarveX(ref layout, 200), "Palette");
+            UI.Custom(() =>
+            {
+                Platform.FillRectangle(new Rectangle(paletteGroup.X, paletteGroup.Y, 64, 64), _currentColor);
+            });
+
+            var previewGroup = UI.ToolGroup(Layout.CarveX(ref layout, 200, Layout.CarvePosition.End), "Preview");
+            UI.Custom(() =>
+            {
+                for (int ty = 0; ty < 3; ++ty)
+                {
+                    for (int tx = 0; tx < 3; ++tx)
+                    {
+                        DrawImage(
+                            new Point(previewGroup.X + (tx * _tileSize), previewGroup.Y + (ty * _tileSize)));
+
+                        DrawImage(
+                            new Point(previewGroup.X + (3 * _tileSize) + 4 + (tx * _tileSize * 2), previewGroup.Y + (ty * _tileSize * 2)),
+                            2);
+                    }
+                }
+            });
+
+            var editorGroup = UI.ToolGroup(layout, "Editor");
+            {
+                var rect = new Rectangle(editorGroup.X, editorGroup.Y,
+                    3 * _tileSize * _pixelSize,
+                    3 * _tileSize * _pixelSize);
+                if (rect.Contains(UI.Platform.MousePosition))
+                {
+                    var px = ((UI.Platform.MousePosition.X - rect.X) / _pixelSize) % _image.Width;
+                    var py = ((UI.Platform.MousePosition.Y - rect.Y) / _pixelSize) % _image.Height;
+                    if (UI.Platform.IsMouseLeftButtonDown())
+                    {
+                        if (UI.Platform.IsAltKeyDown())
+                        {
+                            _currentColor = _image.Get(new Point(px, py));
+                        }
+                        else
+                        {
+                            _image.Set(new Point(px, py), _currentColor);
+                        }
+                    }
+                    else if (UI.Platform.IsMouseRightButtonDown())
+                    {
+                        _image.Set(new Point(px, py), Color.TransparentBlack);
+                    }
+                }
+
+                UI.Custom(() =>
+                {
+                    for (int ty = 0; ty < 3; ++ty)
+                    {
+                        for (int tx = 0; tx < 3; ++tx)
+                        {
+                            DrawImage(
+                                new Point(
+                                    rect.X + (tx * _tileSize * _pixelSize),
+                                    rect.Y + (ty * _tileSize * _pixelSize)),
+                                _pixelSize);
+                            Platform.DrawRectangle(new Rectangle(
+                                rect.X + (tx * _tileSize * _pixelSize),
+                                rect.Y + (ty * _tileSize * _pixelSize),
+                                _tileSize * _pixelSize,
+                                _tileSize * _pixelSize), new Color(0, 0, 0, 0.2f));
+                        }
+                    }
+                    Platform.DrawRectangle(rect, Color.Black);
+                });
+            }
+
+
+            return OpState.Running;
+        }
+
+        private void DrawImage(Point dest, int scale = 1)
+        {
+            for (int y = 0; y < _image.Height; ++y)
+            {
+                for (int x = 0; x < _image.Width; ++x)
+                {
+                    Platform.FillRectangle(
+                        new Rectangle(dest.X + (x * scale), dest.Y + (y * scale), scale, scale),
+                        _image.Get(new Point(x, y)));
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            UI.Draw();
+        }
+    }
+}
---
+++ src/Main.cs
@@ -0,0 +1,61 @@
+﻿using GameLib;
+using SPFSharp;
+using System;
+using GameLib.Graphics;
+using Vector4 = System.Numerics.Vector4;
+
+namespace Tiler
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Game.Run();
+        }
+    }
+
+    static class Game
+    {
+        public static int Width = 1280;
+        public static int Height = 720;
+        public static Rectangle Screen => new Rectangle(0, 0, Width, Height);
+
+        public static SPF.Texture FontTexture;
+        public static PixelFont Font;
+
+        private static void DrawFont(Rectangle dest, Rectangle source, Color color)
+        {
+            SPF.Renderer.DrawTexture(FontTexture,
+                dest.X, dest.Y, dest.Width, dest.Height,
+                source.X, source.Y, source.Width, source.Height,
+                false, false, color, Vector4.Zero);
+        }
+
+        public static void Run()
+        {
+            Platform.ContentFolder = "";
+            using (SPF.Open("Pixel Editor", Width, Height))
+            {
+                FontTexture = new SPF.Texture(Platform.ReadFile("font.png"));
+                Font = new PixelFont(FontTexture.Height, FontTexture.Width / FontTexture.Height, DrawFont) { Scale = 2 };
+
+                var process = new Process();
+                process.Run(new Editor());
+
+                while (SPF.BeginLoop(out float dt))
+                {
+                    dt = Math.Min(dt, 1 / 30f);
+
+                    process.Update(dt);
+
+                    SPF.Renderer.Begin();
+                    process.Draw();
+
+                    SPF.EndLoop();
+                }
+            }
+        }
+
+    }
+}
